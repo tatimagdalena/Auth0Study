b95dbe6153238940c9163f907d553d9ca67408f0
Tatiana <tatimagdalena@gmail.com>
2018-05-10 11:12:39 -0300

    Remove Lock usage from AppDelegate

--- POCAuth0/POCAuth0/AppDelegate.swift
+++ POCAuth0/POCAuth0/AppDelegate.swift
@@ -6,17 +6,14 @@
 //  Copyright © 2018 Tatiana Magdalena. All rights reserved.
 //
 
+import Auth0
 import UIKit
-import Lock
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
     var window: UIWindow?
     
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
         window = UIWindow(frame: UIScreen.main.bounds)
         let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as! ViewController
         viewController.authentication = Auth0Wrapper()
@@ -26,10 +23,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey: Any]) -> Bool {
         print("💡 Asked to open url: \(url.absoluteString)")
-        return Lock.resumeAuth(url, options: options)
+        return Auth0.resumeAuth(url, options: options)
     }
-    
 }
-
